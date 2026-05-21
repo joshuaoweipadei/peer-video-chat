2831cdd992319f799f3477fd04d74cdb68b1a0c9
joshuaoweipadei <oweipadeijoshie@gmail.com>
2026-05-21 04:51:25 -0400

    update including socket.io

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -97,10 +97,15 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 8000;
-server.listen(PORT, () => {
-  console.log(`\n🚀 PeerChat server running on http://localhost:${PORT}\n`);
-});
+if (process.env["NODE_ENV"] !== "production") {
+  const PORT = process.env.PORT || 8000;
+  server.listen(PORT, () => {
+    console.log(`\n🚀 PeerChat server running on http://localhost:${PORT}\n`);
+  });
+}
+
+// // Vercel serverless export
+// export default app;
 
 // import express, { Request, Response, NextFunction } from 'express'
 // import cors from 'cors'
